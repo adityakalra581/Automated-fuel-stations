a3e2d58d4d2309966efab738bb01af242ea84019
adityakalra581 <adityakalra581@gmail.com>
2020-02-24 16:53:19 +0530

    Package Structure Completed

---
+++ CODE/flask_blog/models.py
@@ -0,0 +1,22 @@
+
+from flask_blog import db
+
+
+
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+   
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+
+### Class User here will create an sql table present inside site.db
---
+++ CODE/flask_blog/routes.py
@@ -0,0 +1,36 @@
+
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_blog import app
+from flask_blog.forms import RegistrationForm, LoginForm
+from flask_blog.models import User
+
+
+@app.route('/')
+def home():
+    return render_template('home.html',title='HOME')
+
+
+@app.route('/about')
+def about():
+	return render_template('about.html',title='about')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
