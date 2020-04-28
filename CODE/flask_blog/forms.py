868af5fbfe7c0e11861c3526aa6808ae4ad78e03
adityakalra581 <adityakalra581@gmail.com>
2020-04-29 01:48:54 +0530

    Profile picture and Update

--- CODE/flask_blog/forms.py
+++ CODE/flask_blog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed   ## For uploading different file formats.
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_blog.models import User
@@ -41,3 +43,30 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+## For updating profile there and then.
+
+class UpdateProfileForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+## For updating the profile picture and the allowed extensions right now are jpg and png.
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+
--- CODE/flask_blog/routes.py
+++ CODE/flask_blog/routes.py
@@ -1,7 +1,9 @@
-
+import secrets
+import os
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flask_blog import app, db, bcrypt
-from flask_blog.forms import RegistrationForm, LoginForm
+from flask_blog.forms import RegistrationForm, LoginForm, UpdateProfileForm
 from flask_blog.models import User
 from flask_login import login_user,current_user, logout_user, login_required
 
@@ -79,10 +81,39 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/profile")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    ## This will automatically reduce the size before saving.
+    ## Neccessaery for saving space and making website faster.
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route("/profile", methods=['GET', 'POST'])
 @login_required
 def profile():
-    return render_template('profile.html',title='Profile')
+    form = UpdateProfileForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Profile has been updated!','success')
+        return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email 
+    image_file = url_for('static', filename = 'profile_pics/'+ current_user.image_file)
+    return render_template('profile.html',title='Profile',image_file=image_file,form = form )
 
 ## @login_required will make sure that "/account" page will prompt the user to log in
 ## if already not logged in.
